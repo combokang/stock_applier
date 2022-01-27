a08787eb7a3df61a9872109a1bf9d9767002da70
Combo <hiromoto1995@gmail.com>
2022-01-27 17:12:08 +0800

    sender改為不讀取txt，全部資料從crawler讀取
    優化crawler code寫法

--- crawler.py
+++ crawler.py
@@ -8,9 +8,9 @@ import time
 
 # 若今天為星期日則不執行爬蟲
 if datetime.date.today().weekday() == 6:
-    sys.exit("星期日不執行")
+    sys.exit("星期日程式不執行")
 
-print("開始抓取網頁資料，請稍後...")
+print("開始抓取網頁資料，請稍後...\n")
 
 # 增加亂數延遲，模擬使用者
 time.sleep(random.randint(0, 10))
@@ -20,17 +20,17 @@ url = "https://histock.tw/stock/public.aspx"  # 資料來源：嗨投資 網站
 data = requests.get(url, headers={
                     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36"})
 soup = BeautifulSoup(data.text, "html.parser")
-org_list = soup.find("table").find_all("tr")  # 原始股票列表的html(首列為標題列)
+html_list = soup.find("table").find_all("tr")  # 原始股票列表的html(首列為標題列)
 
 
 # 處理html、篩選欄位，建立股票資料
 stock_list = []  # 處理過的股票列表(二階巢狀)
-for stock in org_list[1:]:  # for迴圈處理每筆股票的html、篩選欄位
+for stock in html_list[1:]:  # for迴圈處理每筆股票的html、篩選欄位
     td = stock.find_all("td")
     name = td[1].find("a").string.replace("\xa0", " ")  # [0]股票代號+名稱(字串)
-    selling_price = td[6].string.strip()  # [1]承銷價(字串)
+    selling_price = float(td[6].string.strip())  # [1]承銷價
     try:
-        gain = td[8].find("span").string.replace(",", "")  # [2]申購總獲利(字串)
+        gain = int(td[8].find("span").string.replace(",", ""))  # [2]申購總獲利
     except:
         gain = "0"
     try:
@@ -40,36 +40,39 @@ for stock in org_list[1:]:  # for迴圈處理每筆股票的html、篩選欄位
     single_stock = [name, selling_price, gain, state]   # 單筆股票資訊
     stock_list.append(single_stock)  # 將單筆股票加入處理結果
 
+
 # 從config讀取篩選條件
 config = configparser.ConfigParser()
 config.read("config.ini", encoding="utf-8")
+
 
 # 將搜尋結果寫入stock_list.txt檔案
 with open("stock_list.txt", mode="w", encoding="utf-8") as file:
     # 寫入篩選條件與文字敘述
     try:
-        max_price = float(config["parameters"]["max_price"])  # 讀取最高承銷價設定
+        # 讀取最高承銷價設定
+        max_price = float(config["parameters"]["max_price"])
         if max_price == 0:
-            str = "最高承銷價: 不進行篩選"
-            print(str)
-            file.write(str + "\n")
+            PRICE_FILTER_STR = "最高承銷價: 不進行篩選"
         else:
-            str = f"最高承銷價: {max_price}"
-            print(str)
-            file.write(str + "\n")
-        min_gain = int(config["parameters"]["min_gain"])  # 讀取最低總獲利設定
-        str = f"最低總獲利: {min_gain}"
-        print(str)
-        file.write(str + "\n")
-        obj_state = config["parameters"]["obj_state"]  # 讀取狀態設定
+            PRICE_FILTER_STR = f"最高承銷價: {max_price}"
+        print(PRICE_FILTER_STR)
+        file.write(PRICE_FILTER_STR + "\n")
+
+        # 讀取最低總獲利設定
+        min_gain = int(config["parameters"]["min_gain"])
+        GAIN_FILTER_STR = f"最低總獲利: {min_gain}"
+        print(GAIN_FILTER_STR)
+        file.write(GAIN_FILTER_STR + "\n")
+
+        # 讀取狀態設定
+        obj_state = config["parameters"]["obj_state"]
         if obj_state == "0":
-            str = "狀態: 不進行篩選"
-            print(str)
-            file.write(str + "\n")
+            STATE_FILTER_STR = "狀態: 不進行篩選"
         else:
-            str = f"狀態: {obj_state}"
-            print(str)
-            file.write(str + "\n")
+            STATE_FILTER_STR = f"狀態: {obj_state}"
+        print(STATE_FILTER_STR)
+        file.write(STATE_FILTER_STR + "\n")
     except:
         print("參數設定有誤，請開啟config.ini設定檔進行檢查")
     print("以上參數可使用記事本開啟config.ini設定檔進行設定\n")
@@ -79,23 +82,25 @@ with open("stock_list.txt", mode="w", encoding="utf-8") as file:
     file.write(title + "\n")
 
     # 寫入股票
-    COUNT = 0  # 篩選結果筆數
-    for line in stock_list:  # 每筆股票
-        if min_gain <= int(line[2]):  # 進行最低總獲利篩選
-            # 進行最高承銷價篩選，且價格不符
-            if max_price != 0 and max_price < float(line[1]):
+    COUNT = 0  # 結果筆數
+    SEARCH_RESULT = []  # 篩選結果
+    for stock in stock_list:  # 每筆股票
+        if stock[2] >= min_gain:  # 總獲利>=進行最低總獲利
+            # 有進行最高承銷價篩選，且承銷價>=最高承銷價，則跳過不選
+            if max_price != 0 and stock[1] >= max_price:
                 continue
             else:
-                if obj_state != "0" and line[3] not in obj_state.split(
-                    ","
-                ):  # 進行狀態篩選，且狀態不符
+                # 有進行狀態篩選，且狀態不符，則跳過不選
+                if obj_state != "0" and stock[3] not in obj_state.split(","):
                     continue
-                else:
-                    line_str = "\t".join(line)
-                    print(line_str)
-                    file.write(line_str + "\n")
+                else:   # 全部條件皆符合則加入搜尋結果、印出且寫入txt檔案備存
+                    SEARCH_RESULT.append(stock)  # 加入篩選結果
+                    stock_str = f"{stock[0]}\t{stock[1]}\t{stock[2]}\t{stock[3]}"
+                    print(stock_str)
+                    file.write(stock_str + "\n")
                     COUNT += 1
+
     # 將最終筆數寫入檔案並印出
-    str = f"共 {COUNT} 筆結果"
+    str = f"共 {COUNT} 筆結果\n"
     print(str)
     file.write(str)
--- sender.py
+++ sender.py
@@ -7,7 +7,7 @@ import crawler
 
 today_str = datetime.date.today().strftime("%Y/%m/%d")
 
-# 讀取config中的篩選條件
+# 讀取config中的email設定
 config = configparser.ConfigParser()
 config.read("config.ini", encoding="utf-8")
 receiver = config["email"]["receiver"]
@@ -17,26 +17,27 @@ msg = email.message.EmailMessage()
 msg["From"] = "stockapplyhelper@gmail.com"
 msg["To"] = "stockapplyhelper@gmail.com"
 msg["BCC"] = receiver
-msg["Subject"] = f"【股票申購助手】{today_str} 股票申購標的"
 
-# 信件html內文列出符合條件的股票，若結果為0筆則以文字取代表格進行提示
-with open("stock_list.txt", mode="r", encoding="utf-8") as file:
-    readlines = file.readlines()
-    htmlmsg = f"""
-    <p>{readlines[0]}<br>{readlines[1]}<br>{readlines[2]}<br>符合條件的可申購股票:</p>"""
-    if crawler.COUNT == 0:  # 篩選結果筆數
-        htmlmsg += "<p>今日無符合條件的股票可申購</p>"
-    else:
-        htmlmsg += """<table border=\"1\" style=\"border-collapse: collapse\">
-        <tr><th>股票名稱</th><th>承銷價</th><th>總獲利</th><th>狀態</th></tr>"""
-        for line in readlines[5:-1]:
-            htmlmsg += "<tr>"
-            inline = line.split("\t")
-            for item in inline:
-                htmlmsg += f"<td>{item}</td>"
-            htmlmsg += "</tr>"
-        htmlmsg += "</table>"
-        htmlmsg += f"<p>{readlines[-1]}</p>"
+# 依結果是否為0筆決定信件標題，且0筆時以文字取代表格進行提示
+# 信件html內文列出 篩選條件 和 符合條件的股票
+htmlmsg = f"<p>{crawler.PRICE_FILTER_STR}<br>{crawler.GAIN_FILTER_STR}<br>{crawler.STATE_FILTER_STR}</p>"
+if crawler.COUNT == 0:  # 若結果為0筆
+    msg["Subject"] = f"【股票申購助手】{today_str} 今日無符合條件的股票可申購"
+    htmlmsg += "<p>今日無符合條件的股票可申購</p>"
+else:
+    msg["Subject"] = f"【股票申購助手】{today_str} 股票申購標的"
+    htmlmsg += """
+    <p>符合條件的可申購股票:</p>
+    <table border=\"1\" style=\"border-collapse: collapse\">
+    <tr><th>股票名稱</th><th>承銷價</th><th>總獲利</th><th>狀態</th></tr>"""
+    for line in crawler.SEARCH_RESULT:
+        htmlmsg += "<tr>"
+        for item in line:
+            htmlmsg += f"<td>{item}</td>"
+        htmlmsg += "</tr>"
+    htmlmsg += "</table>"
+    htmlmsg += f"<p>共 {crawler.COUNT} 筆結果</p>"
+
 msg.add_alternative(htmlmsg, subtype="html")
 
 # 連接gmail server
